509f8df588d9364f22fb931dc13787f2e26631b4
vlahdu <69470698+vlahdu@users.noreply.github.com>
2024-06-26 15:23:48 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,19 +1,9 @@
-from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
-from .forms import LoginForm
-
-app_name = 'core'
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Home page
-    path('signup/', views.signup, name='signup'),  # User signup
-    path('rate/<int:video_id>/', views.rate_private_video, name='rate_private_video'),  # Rate a private video
-    path('rate_public/<int:video_id>/', views.rate_public_video, name='rate_public_video'),  # Rate a public video
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),  # User login
-    path('logout/', views.logout_view, name='logout'),  # User logout
-    path('generate_video/', views.generate_video, name='generate_video'),  # Generate a video
-    path('toggle_visibility/<int:video_id>/', views.toggle_visibility, name='toggle_visibility'),  # Toggle video visibility
-    path('public_videos/', views.public_videos, name='public_videos'),  # List of public videos
-    path('delete_video/<int:video_id>/', views.delete_video, name='delete_video'),  # Delete a video
-]
+    path('', include('core.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
